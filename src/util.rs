9571069c08d6d739e7d9aa713825bcdff1e28964
Jeremiah <jeremiahcrosby@gmail.com>
2020-03-22 11:52:41 -0400

    Cleaner low_byte function

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ use crate::types::*;
 use std::convert::TryInto;
 
 pub fn low_byte(word: Word) -> Byte {
-    ((word & (0xff as u16)) as u8).try_into().unwrap()
+    word.to_be_bytes()[1]
 }
 
 pub fn low_12(word: Word) -> Word {
