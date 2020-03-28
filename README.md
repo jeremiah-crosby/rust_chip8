66583119f8758e939b1c6def91a9a04e7dffff77
Jeremiah <jeremiahcrosby@gmail.com>
2020-03-28 19:24:42 -0400

    Add help

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust CHIP 8 Emulator
+
+Chip 8 emulator written in Rust. Graphics and audio use SDL2, statically linked.
+
+Usage: `rust_chip8 rom_file`
--- src/main.rs
+++ src/main.rs
@@ -13,41 +13,17 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    if args.len() != 2 || args[1] == "-h" {
+        print_usage();
+        return;
+    }
+
     let sdl_context = sdl2::init().unwrap();
     let mut vm = vm::VirtualMachine::new(&sdl_context);
     let rom_path = &args[1];
     vm.run(rom_path.to_string());
-    // let video_subsystem = sdl_context.video().unwrap();
+}
 
-    // let window = video_subsystem
-    //     .window("rust-sdl2 demo", 800, 600)
-    //     .position_centered()
-    //     .build()
-    //     .unwrap();
-    // let mut canvas = window.into_canvas().build().unwrap();
-    // canvas.set_draw_color(Color::RGB(0, 0, 0));
-    // canvas.clear();
-    // canvas.present();
-    // let mut event_pump = sdl_context.event_pump().unwrap();
-    // let mut i = 0;
-    // 'running: loop {
-    //     i = (i + 1) % 255;
-    //     canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-    //     canvas.clear();
-    //     for event in event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit { .. }
-    //             | Event::KeyDown {
-    //                 keycode: Some(Keycode::Escape),
-    //                 ..
-    //             } => break 'running,
-    //             _ => {}
-    //         }
-    //     }
-    //     // The rest of the game loop goes here...
-
-    //     canvas.present();
-    //     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    //}
+fn print_usage() {
+    println!("Usage: rust_chip8 rom_file")
 }
